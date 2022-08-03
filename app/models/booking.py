0ff66227a005d4e94e411e97840a6fae66fa5ce7
Abel Tesfa <abeltesfa@gmail.com>
2022-08-03 10:39:40 -0400

    booking model finished

--- app/models/booking.py
+++ app/models/booking.py
@@ -5,3 +5,10 @@ from .user import User
 
 class Booking(db.Model):
     __tablename__ = 'bookings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    carId = db.Column(db.Integer, db.ForeignKey('cars.id'), nullable=False)
+    startDate = db.Column(db.DateTime, nullable=False)
+    endDate = db.Column(db.DateTime, nullable=False)
+    createdAt = db.Column(DateTime(timezone=True), server_default=func.now())
