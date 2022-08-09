f8481a36a391c7589ada49e919bf7cd563396338
Abel Tesfa <abeltesfa@gmail.com>
2022-08-09 05:16:35 -0400

    date figured out

--- react-app/src/components/BookingDetails/index.js
+++ react-app/src/components/BookingDetails/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteBooking } from "../../store/bookings";
 import BookingEdit from "../BookingEdit";
+import { formatInTimeZone } from 'date-fns-tz';
 
 
 function BookingDetails({ booking, userId, carId }) {
@@ -12,12 +13,17 @@ function BookingDetails({ booking, userId, carId }) {
         await dispatch(deleteBooking(bookingId))
     }
 
+    const convertedStartDate = formatInTimeZone(new Date(booking?.startDate), 'UTC', 'EEEE MMMM do yyyy');
+    const convertedEndDate = formatInTimeZone(new Date(booking?.endDate), 'UTC', 'EEEE MMMM do yyyy');
+
+
+
     return (
         <div>
             {!showEditBooking ?
                 <div>
-                    <p>Start Date: {booking.startDate} </p>
-                    <p>End Date: {booking.endDate}</p>
+                    <p>Start Date: {convertedStartDate} </p>
+                    <p>End Date: {convertedEndDate}</p>
                     <button onClick={() => setShowEditBooking(true)}>Edit Booking</button>
                     <button onClick={()=> bookingOnDelete(booking.id)}>Delete Booking</button>
                 </div>
