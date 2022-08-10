8852e2b571a75ddc29516ac760427c7bacb1d92d
Abel Tesfa <abeltesfa@gmail.com>
2022-08-10 11:02:10 -0400

    validations started on booking

--- react-app/src/components/CarDetails/index.js
+++ react-app/src/components/CarDetails/index.js
@@ -4,8 +4,8 @@ import { NavLink, useHistory, useParams } from "react-router-dom";
 import { addBookings, getCarBookings } from "../../store/bookings";
 import { deleteCar, getAllCars } from "../../store/cars";
 import BookingDetails from "../BookingDetails";
-
-
+import ErrorModal from "../ErrorModal";
+// import { formatInTimeZone } from 'date-fns-tz';
 
 const CarDetails = ({ sessionUser }) => {
     const dispatch = useDispatch();
@@ -19,9 +19,14 @@ const CarDetails = ({ sessionUser }) => {
 
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
+    const [showModal, setShowModal] = useState(false);
     // const [editBookingForm, setEditBookingForm] = useState(false);
-    const [hasSubmitted, setHasSubmitted] = useState(false);
+    // const [hasSubmitted, setHasSubmitted] = useState(false);
     const [validationErrors, setValidationErrors] = useState([]);
+
+    // const convertedToday = formatInTimeZone(new Date(), 'UTC', 'yyyy-MM-dd');
+    // const today = new Date();
+    // console.log(convertedToday)
 
     useEffect(() => {
         dispatch(getAllCars())
@@ -46,14 +51,21 @@ const CarDetails = ({ sessionUser }) => {
     const onSubmit = async (e) => {
         e.preventDefault();
 
-        setHasSubmitted(true);
-        if (validationErrors.length) return alert(`Cannot Submit`);
+        // setHasSubmitted(true);
+        if (validationErrors.length){
+            setShowModal(true)
+        } else{
+            const createdBooking = await dispatch(addBookings(userId, carId, startDate, endDate))
+            if(createdBooking) {
+                setStartDate('');
+                setEndDate('');
+            }
+        }
 
-        await dispatch(addBookings(userId, carId, startDate, endDate))
 
         return () => {
 
-            setHasSubmitted(false);
+            // setHasSubmitted(false);
         }
     }
 
@@ -96,7 +108,7 @@ const CarDetails = ({ sessionUser }) => {
                 </div>
                 <div>
                     <h2>Create Booking</h2>
-                    {hasSubmitted && validationErrors.length > 0 && (
+                    {/* {hasSubmitted && validationErrors.length > 0 && (
                         <div>
                             The following errors were found:
                             <ul>
@@ -105,7 +117,8 @@ const CarDetails = ({ sessionUser }) => {
                                 ))}
                             </ul>
                         </div>
-                    )}
+                    )} */}
+                    <ErrorModal hideModal={() => setShowModal(false)} showModal={showModal} validationErrors={validationErrors} />
                     <form onSubmit={onSubmit}>
                         <div>
                             <label htmlFor="startDate">Start Date:</label>
