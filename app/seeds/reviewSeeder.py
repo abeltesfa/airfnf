47d55d26ca01cb5a4f48b07767a15dff1394fbb4
Abel Tesfa <abeltesfa@gmail.com>
2022-09-02 11:37:02 -0400

    review read state fixed

--- app/seeds/reviewSeeder.py
+++ app/seeds/reviewSeeder.py
@@ -3,7 +3,7 @@ from app.models import db, Review
 def seed_reviews():
     review1 = Review(userId=5, carId=1, rating=5, body='Very reliable car, I recommend the upgrade :D ')
 
-    db.session(review1)
+    db.session.add(review1)
 
     db.session.commit()
 
